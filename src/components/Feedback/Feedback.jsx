da89ac37c23ca5167f8ce5397585ef49c4a2c9a9
Maksym Shevchuk <maksymshevchuk89@gmai.com>
2025-03-07 18:22:16 +0100

    add index file

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -4,17 +4,11 @@ export default function Feedback({ feedback, totalFeedback, positivePercentage }
   return (
     <div className={styles.stats}>
       <h2>Feedback Statistics</h2>
-      {totalFeedback > 0 ? (
-        <>
-          <p>Good: {feedback.good}</p>
-          <p>Neutral: {feedback.neutral}</p>
-          <p>Bad: {feedback.bad}</p>
-          <p>Total: {totalFeedback}</p>
-          <p>Positive feedback: {positivePercentage}%</p>
-        </>
-      ) : (
-        <p>No feedback given</p> 
-      )}
+      <p>Good: {feedback.good}</p>
+      <p>Neutral: {feedback.neutral}</p>
+      <p>Bad: {feedback.bad}</p>
+      <p>Total: {totalFeedback}</p>
+      <p>Positive feedback: {positivePercentage}%</p>
     </div>
   );
 }
